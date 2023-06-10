45997d23a973cf648b8714eabc609b3488d6e942
suhunlin <lin.suhun@msa.hinet.net>
2023-06-10 19:58:25 +0800

    Add MainActivity layout

--- app/src/main/java/com/suhun/servicebind/MainActivity.java
+++ app/src/main/java/com/suhun/servicebind/MainActivity.java
@@ -3,6 +3,7 @@ package com.suhun.servicebind;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void connectServiceFun(View view){
+
+    }
+
+    public void disConnectServiceFun(View view){
+
+    }
+
+    public void getLuckNumberFun(View view){
+
     }
 }

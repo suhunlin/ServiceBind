5a9c72b55b257702f1d0dfda18ae72c3785551bf
suhunlin <lin.suhun@msa.hinet.net>
2023-06-10 20:06:22 +0800

    Add Service bind code and logic.

---
+++ app/src/main/java/com/suhun/servicebind/MyService.java
@@ -0,0 +1,49 @@
+package com.suhun.servicebind;
+
+import android.app.Service;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.Binder;
+import android.os.IBinder;
+import android.util.Log;
+
+import java.util.Random;
+
+public class MyService extends Service {
+    private String tag = MyService.class.getSimpleName();
+    private boolean isBind;
+    private Binder mBinder;
+
+    public MyService() {
+        Log.d(tag, "+++++MyService was born+++++");
+        mBinder = new LocalService();
+        isBind = false;
+    }
+
+    private class LocalService extends Binder{
+        public LocalService(){
+            Log.d(tag, "+++++LocalService was born+++++");
+        }
+        MyService getService(){
+            return  MyService.this;
+        }
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        Log.d(tag, "+++++MyService onBind+++++");
+        isBind = true;
+        return mBinder;
+    }
+
+    @Override
+    public void unbindService(ServiceConnection conn) {
+        Log.d(tag, "+++++MyService unBind+++++");
+        isBind = false;
+        super.unbindService(conn);
+    }
+
+    public int createLuckyNum(){
+        return new Random().nextInt(49) + 1;
+    }
+}

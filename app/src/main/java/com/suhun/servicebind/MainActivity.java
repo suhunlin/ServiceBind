ddcfbf7f80573aedda21e802e274ab50880345ac
suhunlin <lin.suhun@msa.hinet.net>
2023-06-10 19:45:19 +0800

    Initial original code

---
+++ app/src/main/java/com/suhun/servicebind/MainActivity.java
@@ -0,0 +1,14 @@
+package com.suhun.servicebind;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
